ff29210a51a9fa730dff569dc59e63d072743945
slyofzero <sly.of.zero@gmail.com>
2024-12-19 09:16:18 +0530

    Updated /start msg

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -2,6 +2,8 @@ import { BOT_USERNAME } from "@/utils/env";
 import { CommandContext, Context } from "grammy";
 
 export async function startBot(ctx: CommandContext<Context>) {
-  const text = `*Welcome to ${BOT_USERNAME}!!!*\n\n`;
-  await ctx.reply(text);
+  const text = `*Welcome to ${BOT_USERNAME}!!!*\n\n
+
+To perform shadowban check - /checkban`;
+  await ctx.reply(text, { parse_mode: "MarkdownV2" });
 }
